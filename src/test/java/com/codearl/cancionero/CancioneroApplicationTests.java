e8d5362fde1ccf755ab0a4540e94c387e5f2ba01
Leonardo Castro <ldcmleo19@gmail.com>
2025-04-15 19:13:42 -0500

    init project

---
+++ src/test/java/com/codearl/cancionero/CancioneroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codearl.cancionero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancioneroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
